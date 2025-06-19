a2f4953a77802b556bf9c9dc1f7a7bdf459aff9d
Cristobal Carreño <ccarreno@aikologic.com>
2025-06-19 08:46:11 -0400

    changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,32 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'coffebar/neovim-project',
+    opts = {
+      projects = { -- define project roots
+        '~/projects/*',
+        '~/.config/*',
+        '/mnt/e/Documentos/AIKO/Projects/*',
+      },
+      picker = {
+        type = 'telescope', -- or "fzf-lua"
+      },
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append 'globals' -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      -- optional picker
+      { 'nvim-telescope/telescope.nvim', tag = '0.1.4' },
+      -- optional picker
+      { 'ibhagwan/fzf-lua' },
+      { 'Shatur/neovim-session-manager' },
+    },
+    lazy = false,
+    priority = 100,
+  },
+}
