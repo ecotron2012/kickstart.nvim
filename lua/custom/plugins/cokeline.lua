84d621a484ae70369641ba07a5f6fa9ea30b910a
Cristobal Carreño <ccarreno@aikologic.com>
2025-06-21 13:53:57 -0400

    new plugins and visual changes

---
+++ lua/custom/plugins/cokeline.lua
@@ -0,0 +1,82 @@
+return {
+  'willothy/nvim-cokeline',
+  dependencies = {
+    'nvim-lua/plenary.nvim', -- Required for v0.4.0+
+    'nvim-tree/nvim-web-devicons', -- If you want devicons
+    'stevearc/resession.nvim', -- Optional, for persistent history
+  },
+  config = function()
+    local get_hex = require('cokeline.hlgroups').get_hl_attr
+
+    local green = vim.g.terminal_color_2
+    local yellow = vim.g.terminal_color_3
+
+    -- Bufferline style
+    require('cokeline').setup {
+      default_hl = {
+        fg = function(buffer)
+          return buffer.is_focused and get_hex('Normal', 'fg') or get_hex('Comment', 'fg')
+        end,
+        bg = function()
+          return get_hex('ColorColumn', 'bg')
+        end,
+      },
+
+      sidebar = {
+        filetype = { 'NvimTree', 'neo-tree' },
+        components = {
+          {
+            text = function(buf)
+              return buf.filetype
+            end,
+            fg = yellow,
+            bg = function()
+              return get_hex('NvimTreeNormal', 'bg')
+            end,
+            bold = true,
+          },
+        },
+      },
+
+      components = {
+        {
+          text = '｜',
+          fg = function(buffer)
+            return buffer.is_modified and yellow or green
+          end,
+        },
+        {
+          text = function(buffer)
+            return buffer.devicon.icon .. ' '
+          end,
+          fg = function(buffer)
+            return buffer.devicon.color
+          end,
+        },
+        {
+          text = function(buffer)
+            return buffer.index .. ': '
+          end,
+        },
+        {
+          text = function(buffer)
+            return buffer.unique_prefix
+          end,
+          fg = get_hex('Comment', 'fg'),
+          italic = true,
+        },
+        {
+          text = function(buffer)
+            return buffer.filename .. ' '
+          end,
+          bold = function(buffer)
+            return buffer.is_focused
+          end,
+        },
+        {
+          text = ' ',
+        },
+      },
+    }
+  end,
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,13 +6,67 @@ return {
   {
     'coffebar/neovim-project',
     opts = {
-      projects = { -- define project roots
+      -- Project directories
+      projects = {
         '~/projects/*',
+        '~/p*cts/*', -- glob pattern is supported
+        '~/projects/repos/*',
+        '~/Projects/*',
         '~/.config/*',
-        '/mnt/e/Documentos/AIKO/Projects/*',
+        '~/work/*',
       },
+      -- Path to store history and sessions
+      datapath = vim.fn.stdpath 'data', -- ~/.local/share/nvim/
+      -- Load the most recent session on startup if not in the project directory
+      last_session_on_startup = true,
+      -- Dashboard mode prevent session autoload on startup
+      dashboard_mode = false,
+      -- Timeout in milliseconds before trigger FileType autocmd after session load
+      -- to make sure lsp servers are attached to the current buffer.
+      -- Set to 0 to disable triggering FileType autocmd
+      filetype_autocmd_timeout = 200,
+      -- Keymap to delete project from history in Telescope picker
+      forget_project_keys = {
+        -- insert mode: Ctrl+d
+        i = '<C-d>',
+        -- normal mode: d
+        n = 'd',
+      },
+      -- Follow symbolic links in glob patterns (affects startup speed)
+      -- "full" or true - follow symlinks in all matched directories
+      -- "partial" - follow symlinks before any matching operators (*, ?, [])
+      -- "none" or false or nil - do not follow symlinks
+      follow_symlinks = 'full',
+
+      -- Overwrite some of Session Manager options
+      session_manager_opts = {
+        autosave_ignore_dirs = {
+          vim.fn.expand '~', -- don't create a session for $HOME/
+          '/tmp',
+        },
+        autosave_ignore_filetypes = {
+          -- All buffers of these file types will be closed before the session is saved
+          'ccc-ui',
+          'gitcommit',
+          'gitrebase',
+          'qf',
+          'toggleterm',
+        },
+      },
+      -- Picker to use for project selection
+      -- Options: "telescope", "fzf-lua"
+      -- Fallback to builtin select ui if the specified picker is not available
       picker = {
         type = 'telescope', -- or "fzf-lua"
+        preview = {
+          enabled = true, -- show directory structure in Telescope preview
+          git_status = true, -- show branch name, an ahead/behind counter, and the git status of each file/folder
+          git_fetch = false, -- fetch from remote, used to display the number of commits ahead/behind, requires git authorization
+          show_hidden = true, -- show hidden files/folders
+        },
+        opts = {
+          -- picker-specific options
+        },
       },
     },
     init = function()
@@ -29,5 +83,50 @@ return {
     },
     lazy = false,
     priority = 100,
+  },
+  {
+    'xiyaowong/transparent.nvim',
+    opts = { -- table: default groups
+      groups = {
+        'Normal',
+        'NormalNC',
+        'Comment',
+        'Constant',
+        'Special',
+        'Identifier',
+        'Statement',
+        'PreProc',
+        'Type',
+        'Underlined',
+        'Todo',
+        'String',
+        'Function',
+        'Conditional',
+        'Repeat',
+        'Operator',
+        'Structure',
+        'LineNr',
+        'NonText',
+        'SignColumn',
+        'CursorLine',
+        'CursorLineNr',
+        'StatusLine',
+        'StatusLineNC',
+        'EndOfBuffer',
+      },
+      -- table: additional groups that should be cleared
+      extra_groups = {},
+      -- table: groups you don't want to clear
+      exclude_groups = {},
+      -- function: code to be executed after highlight groups are cleared
+      -- Also the user event "TransparentClear" will be triggered
+      on_clear = function() end,
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append 'globals' -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {},
+    lazy = false,
   },
 }
