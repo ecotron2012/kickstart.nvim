a8851c1b961f0f484bc80b1251df91816a45f6a6
Cristobal Carreno <cristobal.carrenof@usm.cl>
2025-08-25 23:21:57 -0400

    Added latex support

---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,15 @@
+return {
+  "lervag/vimtex",
+  lazy = false, -- lazy-loading will disable inverse search
+  config = function()
+    vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-xelatex" }
+    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_compiler_latexmk = {
+      aux_dir = "./.latexmk/aux",
+      out_dir = "./.latexmk/out",
+    }
+  end,
+  keys = {
+    { "<localLeader>l", "", desc = "+vimtex" },
+  },
+}
