f86ee3319a6a0faf1f5772b50cfd2894aafd4c8d
Cristobal Carreno <cristblcarreo.f@gmail.com>
2026-05-06 11:09:34 -0400

    Add tags support

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -5,7 +5,6 @@ return {
     vim.g.vimtex_compiler_latexmk_engines = { ['_'] = '-xelatex' }
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_compiler_latexmk = {
-      aux_dir = './.latexmk/aux',
       out_dir = './.latexmk/out',
     }
   end,
