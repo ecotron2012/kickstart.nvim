21588f6c463b45a92229a4797681249c2d0026cc
Cristobal Carreño <cristblcarreo.f@gmail.com>
2026-05-13 19:38:03 -0400

    change cokeline style

--- lua/custom/plugins/cokeline.lua
+++ lua/custom/plugins/cokeline.lua
@@ -13,66 +13,80 @@ return {
     vim.keymap.set('n', 'gB', '<Plug>(cokeline-focus-prev)', { silent = true, desc = 'Go to previous buffer' })
     vim.keymap.set('n', 'gb', '<Plug>(cokeline-focus-next)', { silent = true, desc = 'Go to next buffer' })
 
+    local map = vim.api.nvim_set_keymap
+    local get_visible = require('cokeline/buffers').get_visible
+    local is_picking_focus = require('cokeline/mappings').is_picking_focus
+    local is_picking_close = require('cokeline/mappings').is_picking_close
+
+    local red = vim.g.terminal_color_1
+    local yellow = vim.g.terminal_color_3
+    local oceanblue = '#0066cc'
+
     -- Bufferline style
     require('cokeline').setup {
-      default_hl = {
-        fg = function(buffer)
-          return buffer.is_focused and get_hex('Normal', 'fg') or get_hex('Comment', 'fg')
-        end,
-        bg = function()
-          return get_hex('ColorColumn', 'bg')
-        end,
+      -- Only show the bufferline when there are at least this many visible buffers.
+      -- default: `1`.
+      show_if_buffers_are_at_least = 1,
+
+      buffers = {
+        -- filter_valid = function(buffer) return buffer.filetype ~= 'netrw' and buffer.filetype ~= 'startify' end,
+
+        focus_on_delete = 'next',
+
+        new_buffers_position = 'next',
       },
 
+      mappings = {
+        cycle_prev_next = true,
+      },
+
+      rendering = {
+        max_buffer_width = 999,
+      },
+
+      default_hl = {
+        fg = function(buffer) return buffer.is_focused and get_hex('Normal', 'fg') or get_hex('Comment', 'fg') end,
+        bg = function(buffer) return buffer.is_focused and oceanblue or get_hex('ColorColumn', 'bg') end,
+      },
       sidebar = {
         filetype = { 'NvimTree', 'neo-tree' },
         components = {
           {
-            text = function(buf)
-              return buf.filetype
-            end,
+            text = function(buf) return buf.filetype end,
             fg = yellow,
-            bg = function()
-              return get_hex('NvimTreeNormal', 'bg')
-            end,
+            bg = function() return get_hex('NvimTreeNormal', 'bg') end,
             bold = true,
           },
         },
       },
-
       components = {
         {
-          text = function(buffer)
-            return (buffer.index ~= 1) and '▏' or ''
-          end,
-          fg = function()
-            return get_hex('Normal', 'fg')
-          end,
+          text = function(buffer) return buffer.is_focused and '' or ' ' end,
+          fg = function(buffer) return buffer.is_focused and buffer.index ~= 1 and get_hex('Normal', 'bg') or oceanblue end,
+          bg = function(buffer) return buffer.is_focused and oceanblue or get_hex('Normal', 'bg') end,
+        },
+        {
+          text = function(buffer) return (is_picking_focus() or is_picking_close()) and buffer.pick_letter .. ' ' or buffer.devicon.icon end,
+          fg = function(buffer) return (is_picking_focus() and red) or (is_picking_close() and yellow) or buffer.devicon.color end,
+          style = function(_) return (is_picking_focus() or is_picking_close()) and 'italic,bold' or nil end,
+        },
+        {
+          text = function(buffer) return buffer.index .. ' ' end,
+          style = 'italic',
+        },
+        {
+          text = function(buffer) return buffer.filename .. '  ' end,
+          style = function(buffer) return buffer.is_focused and 'bold' or nil end,
         },
         {
           text = function(buffer)
-            return '    ' .. buffer.devicon.icon
+            if buffer.is_focused then return '' end
+            local buffers = get_visible()
+            local next = buffer.index + 1
+            return buffers[next] and buffers[next].is_focused and '' or ' '
           end,
-          fg = function(buffer)
-            return buffer.devicon.color
-          end,
-        },
-        {
-          text = function(buffer)
-            return buffer.filename .. '    '
-          end,
-          bold = function(buffer)
-            return buffer.is_focused
-          end,
-        },
-        {
-          text = '󰖭',
-          on_click = function(_, _, _, _, buffer)
-            buffer:delete()
-          end,
-        },
-        {
-          text = '  ',
+          fg = function(buffer) return buffer.is_focused and oceanblue or get_hex('Normal', 'fg') end,
+          bg = get_hex('ColorColumn', 'bg'),
         },
       },
     }
