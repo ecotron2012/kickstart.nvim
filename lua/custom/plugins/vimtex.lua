9521aebac387935062a91dced8c1cccd86da6616
Cristóbal Carreño <cristblcarreo.f@gmail.com>
2026-08-14 09:47:19 -0400

    change vimtex output directory

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -3,7 +3,7 @@ vim.pack.add { { src = 'https://github.com/lervag/vimtex', name = 'vimtex' } }
 vim.g.vimtex_compiler_latexmk_engines = { ['_'] = '-xelatex' }
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_latexmk = {
-  out_dir = './',
+  out_dir = './out/',
 }
 
 vim.keymap.set('n', '<localleader>l', function() vim.fn['vimtex#context#menu']() end, { desc = '+vimtex' })
