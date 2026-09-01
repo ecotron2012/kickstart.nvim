d39495846b1c6a2ab7e06674a6320d2c169cc8a3
Cristóbal Carreño <cristblcarreo.f@gmail.com>
2026-09-01 14:37:11 -0400

    performance improvements; faster.nvim added

--- lua/custom/plugins/ale.lua
+++ lua/custom/plugins/ale.lua
@@ -10,4 +10,6 @@ g.ale_ruby_rubocop_auto_correct_all = 1
 g.ale_linters = {
   ruby = { 'rubocop', 'ruby' },
   lua = { 'lua_language_server' },
+  javascript = {'eslint'},
+  javascriptreact = {'eslint'}
 }
